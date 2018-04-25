78255d788875e9cbdf38560d56594255aa42a924
Courtney Baker <clbw8b@mail.missouri.edu>
2018-04-25 15:54:06 -0500

    Final Project First Draft

---
+++ audio.js
@@ -0,0 +1,448 @@
+/*
+Courtney Baker
+ISLT-7375, Mobile Web App Development
+Final Project
+May 3, 2018*/
+
+
+//Audio for Numbers Module 
+ function play1(player){
+                 var hana = document.getElementById(player);
+                 hana.play();
+                 }
+		
+ function play2(player){
+                 var dul = document.getElementById(player);
+                 dul.play();
+                 }		 
+				 
+function play3(player){
+                 var set = document.getElementById(player);
+                 set.play();
+                 }
+
+function play4(player){
+                 var net = document.getElementById(player);
+                 net.play();
+                 }	
+
+function play5(player){
+                 var daseot = document.getElementById(player);
+                 daseot.play();
+                 }
+				 
+function play6(player){
+                 var yeoseot = document.getElementById(player);
+                 yeoseot.play();
+                 }	
+
+function play7(player){
+                 var ilgop = document.getElementById(player);
+                 ilgop.play();
+                 }		 		
+
+function play8(player){
+                 var yeoldeol = document.getElementById(player);
+                 yeoldeol.play();
+                 }		
+
+ function play9(player){
+                 var ahop = document.getElementById(player);
+                 ahop.play();
+                 }		 		
+
+function play10(player){
+                 var yeol = document.getElementById(player);
+                 yeol.play();
+                 }		 
+
+function play11(player){
+                 var il = document.getElementById(player);
+                 il.play();
+                 }	
+
+function play12(player){
+                 var i = document.getElementById(player);
+                 i.play();
+                 }	
+
+function play13(player){
+                 var sam = document.getElementById(player);
+                 sam.play();
+                 }	
+
+function play14(player){
+                 var sa = document.getElementById(player);
+                 sa.play();
+                 }		
+
+function play15(player){
+                 var o = document.getElementById(player);
+                 o.play();
+                 }		 	
+
+function play16(player){
+                 var yuk = document.getElementById(player);
+                 yuk.play();
+                 }		
+
+function play17(player){
+                 var chil = document.getElementById(player);
+                 chil.play();
+                 }		 	
+
+function play18(player){
+                 var pal = document.getElementById(player);
+                 pal.play();
+                 }	
+
+function play19(player){
+                 var gu = document.getElementById(player);
+                 gu.play();
+                 }		
+
+function play20(player){
+                 var sip = document.getElementById(player);
+                 sip.play();
+                 }		
+				 
+//Audio for Greetings Module
+
+function play21(player){
+                 var annyeong = document.getElementById(player);
+                 annyeong.play();
+                 }
+				 
+function play22(player){
+                 var meet = document.getElementById(player);
+                 meet.play();
+                 }
+				 
+function play23(player){
+                 var bye1 = document.getElementById(player);
+                 bye1.play();
+                 }
+				 
+function play24(player){
+                 var bye2 = document.getElementById(player);
+                 bye2.play();
+                 }
+				 
+function play25(player){
+                 var name = document.getElementById(player);
+                 name.play();
+                 }
+				 
+function play26(player){
+                 var myname = document.getElementById(player);
+                 myname.play();
+                 }
+				 
+function play27(player){
+                 var thank = document.getElementById(player);
+                 thank.play();
+                 }
+				 
+function play28(player){
+                 var sorry = document.getElementById(player);
+                 sorry.play();
+                 }
+				 
+//Audio for Colors Module
+				 
+function play29(player){
+                 var black = document.getElementById(player);
+                 black.play();
+                 }
+				 
+function play30(player){
+                 var white = document.getElementById(player);
+                 white.play();
+                 }
+				 
+function play31(player){
+                 var gray = document.getElementById(player);
+                 gray.play();
+                 }
+				 
+function play32(player){
+                 var red = document.getElementById(player);
+                 red.play();
+                 }
+				 
+function play33(player){
+                 var blue = document.getElementById(player);
+                 blue.play();
+                 }
+				 
+function play34(player){
+                 var green = document.getElementById(player);
+                 green.play();
+                 }
+				 
+function play35(player){
+                 var yellow = document.getElementById(player);
+                 yellow.play();
+                 }
+				 
+function play36(player){
+                 var orange = document.getElementById(player);
+                 orange.play();
+                 }	
+
+function play37(player){
+                 var brown = document.getElementById(player);
+                 brown.play();
+                 }
+
+function play38(player){
+                 var pink = document.getElementById(player);
+                 pink.play();
+                 }	
+
+function play39(player){
+                 var purple = document.getElementById(player);
+                 purple.play();
+                 }	
+
+function play40(player){
+                 var gold = document.getElementById(player);
+                 gold.play();
+                 }
+
+function play41(player){
+                 var silver = document.getElementById(player);
+                 silver.play();
+                 }	
+				 
+//Audio for Family Module
+
+function play42(player){
+                 var mom = document.getElementById(player);
+                 mom.play();
+                 }	
+
+function play43(player){
+                 var dad = document.getElementById(player);
+                 dad.play();
+                 }	
+
+function play44(player){
+                 var grandma = document.getElementById(player);
+                 grandma.play();
+                 }	
+
+function play45(player){
+                 var grandpa = document.getElementById(player);
+                 grandpa.play();
+                 }	
+
+function play46(player){
+                 var youngerbrother = document.getElementById(player);
+                 youngerbrother.play();
+                 }	
+
+function play47(player){
+                 var youngersister = document.getElementById(player);
+                 youngersister.play();
+                 }		
+
+function play48(player){
+                 var olderbrother1 = document.getElementById(player);
+                 olderbrother1.play();
+                 }	
+
+function play49(player){
+                 var olderbrother2 = document.getElementById(player);
+                 olderbrother2.play();
+                 }	
+
+function play50(player){
+                 var oldersister1 = document.getElementById(player);
+                 oldersister1.play();
+                 }	
+
+function play51(player){
+                 var oldersister2 = document.getElementById(player);
+                 oldersister2.play();
+                 }		
+
+function play52(player){
+                 var husband = document.getElementById(player);
+                 husband.play();
+                 }		
+
+function play53(player){
+                 var wife = document.getElementById(player);
+                 wife.play();
+                 }	
+
+function play54(player){
+                 var daughter = document.getElementById(player);
+                 daughter.play();
+                 }	
+
+function play55(player){
+                 var son = document.getElementById(player);
+                 son.play();
+                 }	
+				 
+//Audio for Food Module
+
+function play56(player){
+                 var yum = document.getElementById(player);
+                 yum.play();
+                 }	
+
+function play57(player){
+                 var meat = document.getElementById(player);
+                 meat.play();
+                 }				 
+				 
+function play58(player){
+                 var chicken = document.getElementById(player);
+                 chicken.play();
+                 }	
+
+function play59(player){
+                 var beef = document.getElementById(player);
+                 beef.play();
+                 }	
+
+function play60(player){
+                 var pork = document.getElementById(player);
+                 pork.play();
+                 }	
+				 
+function play61(player){
+                 var seafood = document.getElementById(player);
+                 seafood.play();
+                 }	
+				 
+function play62(player){
+                 var bread = document.getElementById(player);
+                 bread.play();
+                 }	
+				 
+function play63(player){
+                 var fruit = document.getElementById(player);
+                 fruit.play();
+                 }	
+				
+function play64(player){
+                 var veggie = document.getElementById(player);
+                 veggie.play();
+                 }
+
+function play65(player){
+                 var beverage = document.getElementById(player);
+                 beverage.play();
+                 }	
+
+function play66(player){
+                 var milk = document.getElementById(player);
+                 milk.play();
+                 }	
+
+function play67(player){
+                 var juice = document.getElementById(player);
+                 juice.play();
+                 }	
+
+function play68(player){
+                 var water = document.getElementById(player);
+                 water.play();
+                 }	
+
+function play69(player){
+                 var cola = document.getElementById(player);
+                 cola.play();
+                 }		
+
+function play70(player){
+                 var beer = document.getElementById(player);
+                 beer.play();
+                 }	
+
+function play71(player){
+                 var coffee = document.getElementById(player);
+                 coffee.play();
+                 }	
+
+function play72(player){
+                 var tea = document.getElementById(player);
+                 tea.play();
+                 }	
+
+function play73(player){
+                 var restaurant = document.getElementById(player);
+                 restaurant.play();
+                 }	
+
+function play74(player){
+                 var menu = document.getElementById(player);
+                 menu.play();
+                 }	
+
+function play75(player){
+                 var order = document.getElementById(player);
+                 order.play();
+                 }	
+
+function play76(player){
+                 var delicious = document.getElementById(player);
+                 delicious.play();
+                 }	
+
+function play77(player){
+                 var notdelicious = document.getElementById(player);
+                 notdelicious.play();
+                 }	
+
+function play78(player){
+                 var spicy = document.getElementById(player);
+                 spicy.play();
+                 }	
+
+function play79(player){
+                 var sweet = document.getElementById(player);
+                 sweet.play();
+                 }	
+
+function play80(player){
+                 var waterplease = document.getElementById(player);
+                 waterplease.play();
+                 }		
+
+function play81(player){
+                 var bill = document.getElementById(player);
+                 bill.play();
+                 }
+
+function play82(player){
+                 var koreanfood = document.getElementById(player);
+                 koreanfood.play();
+                 }	
+
+function play83(player){
+                 var westernfood = document.getElementById(player);
+                 westernfood.play();
+                 }		
+
+function play84(player){
+                 var chinesefood = document.getElementById(player);
+                 chinesefood.play();
+                 }	
+
+function play85(player){
+                 var japanesefood = document.getElementById(player);
+                 japanesefood.play();
+                 }				 
+
+				 
+				 
+			 
+				 
+
+				 
+
